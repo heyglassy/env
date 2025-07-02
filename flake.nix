24edb1db1f357fb843a1cfc0d77367cff3bc5c9d
Christian Glassiognon <63924603+heyglassy@users.noreply.github.com>
2025-07-02 10:37:17 -0700

    updated packages

--- flake.nix
+++ flake.nix
@@ -106,8 +106,8 @@
               cleanup = "zap";   # or "uninstall" if you prefer
             };
 
-            brews = [ "pscale" "go@1.24" "atlas" "temporalio/brew/tcld" ];
-            taps = ["temporalio/brew"];
+            brews = [ "pscale" "go@1.24" "temporalio/brew/tcld" ];
+            taps = ["temporalio/brew" ];
             casks = [ "orbstack" "slack" "arc" "1password" "legcord" "cursor" "ghostty" "raycast" "beeper" "superhuman" "figma" "notion" "hiddenbar" "cloudflare-warp" "notion-calendar" "rectangle" "flux-app" "rescuetime" ];
           };
         }
@@ -173,9 +173,12 @@
               enable = true;                    # activates Home-Manager's Bash module
               package = pkgs.bashInteractive;   # this is Bash 5.2 from nixpkgs
               initExtra = ''
+                export EDITOR=vim
+                export VISUAL=vim
+
                 eval "$(fnm env --use-on-cd --shell bash)"
                 eval "$(/opt/homebrew/bin/brew shellenv)"
-                export PATH="$HOME/go/bin:$PATH"
+                export PATH="/Users/carnegie/go/bin:$PATH"
               '';
             };
 
