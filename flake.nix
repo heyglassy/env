93e5088eb31b9f5f4391a8910e4587217fab9cc0
Christian Glassiognon <63924603+heyglassy@users.noreply.github.com>
2025-09-05 14:04:40 -0700

    Directory nix up arrow thingy

--- flake.nix
+++ flake.nix
@@ -198,6 +198,7 @@
               settings = {
                 auto_sync = true;
                 enter_accept = true;
+                filter_mode_shell_up_key_binding = "directory";
               };
             };
 
